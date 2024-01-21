50848a7aab8b9bbfe02cead3cc2a54cc70d70a2f
Abdullah Sarwar <gmabdullah32@gmail.com>
2024-01-21 13:44:36 +0500

    add with credentials

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:8000',  // Set your default base URL here
-  baseURL: "https://blog-apis-cyan.vercel.app/"
+  baseURL: "https://blog-apis-cyan.vercel.app/",
+  withCredentials: true,
 });
 
 export default instance;

f0b4805ace2d162d33dadc6b51ad2769d0e00ab8
Abdullah Sarwar <gmabdullah32@gmail.com>
2024-01-21 16:37:20 +0500

    swap remote base url

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:8000',  // Set your default base URL here
+  // baseURL: 'http://localhost:8000/api',  // Set your default base URL here
   baseURL: "https://blog-apis-cyan.vercel.app/api",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default instance;

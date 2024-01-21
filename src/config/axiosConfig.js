5a65bbace507d5c353ae0dd30644bd49d794fc2d
Abdullah Sarwar <gmabdullah32@gmail.com>
2024-01-21 12:59:04 +0500

    add deployment url

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',  // Set your default base URL here
+  // baseURL: 'http://localhost:8000',  // Set your default base URL here
+  baseURL: "https://blog-apis-five.vercel.app"
 });
 
 export default instance;
